13e036030c315615e5a0673df9a81d1596e34ca0
mvdvalk <46578571+mwptje@users.noreply.github.com>
2019-10-20 15:13:04 +0200

    small change to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import React from "react";
+import { Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 const HomePage = props => {
   console.log(props);
   return (
     <div>
-      <button onClick={() => props.history.push('/topics')}>Topics </button>
+      <button onClick={() => props.history.push("/topics")}>Topics </button>
       <h1>HOME PAGE</h1>
     </div>
   );
@@ -35,11 +35,9 @@ const TopicDetail = props => {
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/blog/asdqw/topics' component={TopicsList} />
-      <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
-      <Route exact path='/blog/topics' component={TopicsList} />
-      <Route path='/blog/topics/:topicId' component={TopicDetail} />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/topics" component={TopicsList} />
+      <Route path="/topics/:topicId" component={TopicDetail} />
     </div>
   );
 }
